b88a7097e197ce09fa1a8d4cf75e90b77753b0e9
Zelan Zou <zelan.zou@nxp.com>
2025-10-10 11:12:44 +0800

    cmake/fakedriver: Explicitly create target have more stable output when build in multiple platform
    fix error:
    | CMake Error at fakedriver/CMakeLists.txt:29 (add_dependencies):
    |   The dependency target
    |   "/opt/samba/nxa21648/whinlatter/build/tmp/work/armv8a-poky-linux/patrace/5.4.0/sources/patrace-5.4.0/patrace/project/cmake/../../src/fakedriver/gles2/auto.cpp"
    |   of target "GLESv2" does not exist.
    
    Signed-off-by: Zelan Zou <zelan.zou@nxp.com>

--- patrace/project/cmake/fakedriver/src.cmake
+++ patrace/project/cmake/fakedriver/src.cmake
@@ -1,12 +1,32 @@
 add_custom_command (
     OUTPUT ${SRC_ROOT}/fakedriver/egl/auto.cpp
-        ${SRC_ROOT}/fakedriver/gles1/auto.cpp
-        ${SRC_ROOT}/fakedriver/gles2/auto.cpp
     COMMAND ${PYTHON_EXECUTABLE} ${SRC_ROOT}/fakedriver/autogencode.py
     DEPENDS
+        ${SRC_FAKEDRIVER_EGL}
         ${SRC_ROOT}/fakedriver/autogencode.py
     WORKING_DIRECTORY ${SRC_ROOT}/fakedriver
 )
+add_custom_target(egl_auto_src_generation DEPENDS ${SRC_ROOT}/fakedriver/egl/auto.cpp)
+
+add_custom_command (
+    OUTPUT ${SRC_ROOT}/fakedriver/gles1/auto.cpp
+    COMMAND ${PYTHON_EXECUTABLE} ${SRC_ROOT}/fakedriver/autogencode.py
+    DEPENDS
+        ${SRC_FAKEDRIVER_GLES1}
+        ${SRC_ROOT}/fakedriver/autogencode.py
+    WORKING_DIRECTORY ${SRC_ROOT}/fakedriver
+)
+add_custom_target(gles1_auto_src_generation DEPENDS ${SRC_ROOT}/fakedriver/gles1/auto.cpp)
+
+add_custom_command (
+    OUTPUT ${SRC_ROOT}/fakedriver/gles2/auto.cpp
+    COMMAND ${PYTHON_EXECUTABLE} ${SRC_ROOT}/fakedriver/autogencode.py
+    DEPENDS
+        ${SRC_FAKEDRIVER_GLES2_GLES3}
+        ${SRC_ROOT}/fakedriver/autogencode.py
+    WORKING_DIRECTORY ${SRC_ROOT}/fakedriver
+)
+add_custom_target(gles2_auto_src_generation DEPENDS ${SRC_ROOT}/fakedriver/gles2/auto.cpp)
 
 set(SRC_FAKEDRIVER_EGL
     ${SRC_ROOT}/fakedriver/common.cpp
